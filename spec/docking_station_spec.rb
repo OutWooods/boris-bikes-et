a231f76c4c3884a571848ba828b7acd156612af6
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-03 15:51:25 +0000

    Finished challenge 8: defined release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,9 @@
-describe DockingStation do
+require './lib/docking_station.rb'
 
+describe DockingStation do
+  it 'a test' do
+    expect(DockingStation.new).to respond_to :release_bike
+  end
 end
+
+docking_station = DockingStation.new
