5e9ed93f648c1eee0c40e762876e565167e67e5c
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-03 17:54:45 +0000

    Chapter 10: releases a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,17 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
-  it 'a test' do
+  it 'docking station responds' do
     expect(DockingStation.new).to respond_to :release_bike
   end
-end
 
-docking_station = DockingStation.new
+  it 'releases working bikes' do
+    expect(Bike.new).to be_working
+  end
+
+#expect(subject.release_bike).to be_a Bike
+#bike = Bike.new
+#expect(bike).to be_working
+
+
+end
