80446b0e1a72dd8f213ef0cd53797d367206848d
Alexy <alexandra.fina@yahoo.com>
2018-01-04 15:47:12 +0000

    Dock full raise error - test passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-   return raise("No bikes available") if @bike.nil?  
+   return raise("No bikes available") if @bike.nil?
     @bike
   end
 
   def dock_bike(bike)
+    return raise("Dock is full") if !@bike.nil?
     @bike = bike
   end
 end
