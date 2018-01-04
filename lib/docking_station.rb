76f220bb8e0fe2ac858a6c25e8f1c3af9f6a2709
Tagrand <thomas.a.grand@gmail.com>
2018-01-04 15:16:33 +0000

    refactor release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,7 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-   return raise("No bikes available") if @bike == nil  
+   return raise("No bikes available") if @bike.nil?  
     @bike
   end
 
